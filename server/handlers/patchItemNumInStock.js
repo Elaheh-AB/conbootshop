5c7d9e3d83b950393a435fc7509ef9ecc4348722
Elaheh Ahmadi <e.ahmadi.b@gmail.com>
2022-08-02 16:26:39 -0400

    patch item  availability

--- server/handlers/patchItemNumInStock.js
+++ server/handlers/patchItemNumInStock.js
@@ -9,9 +9,11 @@ const options = {
 };
 
 // updates items in the cart with given id
-const updateCart = async (req, res) => {
-  const cartId = req.body._id; //cart id
-  const itemIds = req.body.itemIds; //array of item ids in cart
+const updateItemNum = async (req, res) => {
+  const itemId = parseInt(req.body.itemId); //id of the item
+  //number of purchased items to deducted from available num
+  //purchasedNo can be positive(for adding) or negative (for removing)
+  const purchasedNo = parseInt(req.body.purchasedNo);
   // creates a new client
   const client = new MongoClient(MONGO_URI, options);
   try {
@@ -21,23 +23,23 @@ const updateCart = async (req, res) => {
     // connect to the database (db name is provided as an argument to the function)
     const db = client.db(dbName);
     console.log("connected!");
-
+    //updates the item only if there is availability of that
     const result = await db
-      .collection("cart")
+      .collection("items")
       .updateOne(
-        { _id: `${cartId}` },
-        { $set: { ...req.body, itemIds: itemIds } }
+        { _id: itemId, numInStock: { $gte:-purchasedNo } },
+        { $inc: { numInStock: purchasedNo } }
       );
     // On success/no error, send
     if (result.acknowledged === true && result.modifiedCount === 1) {
       return res
         .status(200)
-        .json({ status: 200, cartId, message: "cart updated successfully" });
+        .json({ status: 200, itemId, message: "stock updated successfully" });
     } else {
       // on failure/error, send
       return res
         .status(404)
-        .json({ status: 404, cartId, message: "can't update the cart" });
+        .json({ status: 404, itemId, message: "can't update the stock" });
     }
   } catch (err) {
     console.log(err.stack);
@@ -48,5 +50,5 @@ const updateCart = async (req, res) => {
 };
 
 module.exports = {
-    updateCart,
+  updateItemNum,
 };
